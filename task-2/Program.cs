47d68d00544649fc8cf95fc621a5b8756fcf048f
zelalem61 <zelalemhab19@gmail.com>
2024-03-09 15:49:53 -0800

    first commit

---
+++ task-2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    public static Dictionary<char, int> CounterOfWord(string word){
+        Dictionary<char, int> counter = new Dictionary<char, int>();
+        foreach (var letter in word)
+        {
+            if (!char.IsLetter(letter)){
+                continue;
+            }
+            else if (counter.ContainsKey(letter))
+            {
+                counter[letter]++;
+            }
+            else
+            {
+                counter.Add(letter, 1);
+            }
+        }
+
+        return counter;
+    }
+
+     static void Main(){
+        var counter = CounterOfWord("Hel',,,lo Worldhhh");
+        foreach (var pair in counter)
+        {
+            Console.WriteLine($"Character: {pair.Key}, Count: {pair.Value}");
+        }
+}
+}
