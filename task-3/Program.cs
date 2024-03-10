57654d5ba81d039d62c058e3aec7da03d6f8d126
zelalem61 <zelalemhab19@gmail.com>
2024-03-09 16:07:11 -0800

    first commit

---
+++ task-3/Program.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+
+class Program
+{
+    public static bool IsPalindrome(string str)
+    {
+        int i = 0, j = str.Length - 1;
+        while (i < j)
+        {
+            if (str[i] != str[j])
+            {
+                return false;
+            }
+            else{
+                i+=1;
+                j-=1;
+            }
+        }
+        return true; 
+    }
+
+    static void Main()
+    {
+        string str = "madkam";
+        Console.WriteLine(IsPalindrome(str)); // True
+    }
+}
